2f5b5d763df4bef37fa121404ee153c5d6818a8b
Tamara Reyda <tamara.reyda@gmail.com>
2014-09-24 03:29:59 -0700

    added ability to display input values, added gitignore file

--- color-viewer.js
+++ color-viewer.js
@@ -1,5 +1,9 @@
 var main = function() {
-	
+	$('#view').click(function() {
+		var hex = $('#color').val();
+		$('<div>').text(hex).prependTo('.swatches');
+		$('#color').val('');
+	});
 };
 
 $(document).ready(main);
